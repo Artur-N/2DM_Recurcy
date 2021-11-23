d89b03ae95b19771d6012e4b5875be124d3a2e95
Artur <arturnl@ya.ru>
2021-11-23 17:24:54 +0300

    Task 01

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,31 +1,47 @@
-﻿// Создание и печать двумерного массива
+﻿// 1. Дан целочисленный двумерный массив, размерности n х m. Найти сумму и произведение всех элементов массива.
 
-void SetArr2D(int[,] a)
+void SetArr2D(int[,] arr)
 {
-    for (int i = 0; i < a.GetLength(0); i++)
+    for (int i = 0; i < arr.GetLength(0); i++)
     {
-        for (int j = 0; j < a.GetLength(1); j++)
+        for (int j = 0; j < arr.GetLength(1); j++)
         {
-            a[i, j] = i * 10 + j;
+            arr[i, j] = i * 2 + j + 1;
         }
     }
 }
 
-void Print(int[,] a)
+void Print(int[,] arr)
 {
-    for (int i = 0; i < a.GetLength(0); i++)
+    for (int i = 0; i < arr.GetLength(0); i++)
     {
-        for (int j = 0; j < a.GetLength(1); j++)
+        for (int j = 0; j < arr.GetLength(1); j++)
         {
-            Console.Write($"{a[i, j],4}");
+            Console.Write($"{arr[i, j],4}");
         }
         Console.WriteLine();
     }
 }
 
-int x = 3;
-int y = 4;
-int[,] a;
-a = new int[x, y];
-SetArr2D(a);
-Print(a);
+void Task01(int[,] arr, out int sum, out int multi)
+{
+    sum = 0;
+    multi = 1;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            sum = sum + arr[i, j];
+            multi = multi * arr[i, j];
+        }
+    }
+}
+int a = 3;
+int b = 3;
+int[,] Array = new int[a, b];
+SetArr2D(Array);
+Print(Array);
+int s, m;
+Task01(Array, out s, out m);
+Console.WriteLine($"Суммы всех элементов массива = {s}");
+Console.WriteLine($"Произведение всех элементов массива = {m}");
