efb97ed17aa66c45e05a223f1cea969a1abc54d7
Artur <arturnl@ya.ru>
2021-11-23 16:10:02 +0300

    Task 01

---
+++ Task01/Program.cs
@@ -0,0 +1,31 @@
+﻿// Создание и печать двумерного массива
+
+void SetArr2D(int[,] a)
+{
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < a.GetLength(1); j++)
+        {
+            a[i, j] = i * 10 + j;
+        }
+    }
+}
+
+void Print(int[,] a)
+{
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < a.GetLength(1); j++)
+        {
+            Console.Write($"{a[i, j],4}");
+        }
+        Console.WriteLine();
+    }
+}
+
+int x = 3;
+int y = 4;
+int[,] a;
+a = new int[x, y];
+SetArr2D(a);
+Print(a);
