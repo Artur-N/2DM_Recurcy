c9c68041bec0c99d3534e38c58fc39254775ca41
Artur <arturnl@ya.ru>
2021-12-02 23:48:45 +0300

    Task 07

---
+++ Task07/Program.cs
@@ -0,0 +1,42 @@
+﻿// 7. Дан целочисленный двумерный массив, размерности n х m. Заменить все элементы на их квадраты.
+
+Random rnd = new Random();
+void SetArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(1, 10);
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j],3}");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Task07(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = (int)Math.Pow(array[i,j],2);
+        }
+    }
+}
+
+int[,] array2D = new int[3,3];
+SetArray2D(array2D);
+PrintArray(array2D);
+Console.WriteLine();
+Task07(array2D);
+PrintArray(array2D);
