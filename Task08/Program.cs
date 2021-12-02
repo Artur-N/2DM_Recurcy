4806e376174a80dd70937eb0ee388f37d00e553c
Artur <arturnl@ya.ru>
2021-12-03 01:10:50 +0300

    Task 08

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -1,4 +1,4 @@
-﻿// Дан целочисленный двумерный массив, размерности n х m. Выяснить, какое число встречается в какой строке раньше — положительное или отрицательное.
+﻿// 8. Дан целочисленный двумерный массив, размерности n х m. Найти среднее арифметическое всех элементов массива.
 
 Random rnd = new Random();
 void SetArray2D(int[,] array)
@@ -7,7 +7,7 @@ void SetArray2D(int[,] array)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            array[i, j] = rnd.Next(-10, 10);
+            array[i, j] = rnd.Next(1, 10);
         }
     }
 }
@@ -23,29 +23,22 @@ void PrintArray(int[,] array)
     }
 }
 
-void Task09(int[,] array)
+double Task08(int[,] array)
 {
+    double average;
+    double sum = 0;
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            if (array[i,j] > 0)
-            {
-                Console.WriteLine($"Положительное {i+1}");
-                break;
-            }
-            else if (array[i,j] < 0)
-            {
-                Console.WriteLine($"Отрицательное {i+1}");
-                break;
-            }
+            sum = sum + array[i, j];
         }
     }
+    average = sum/array.Length;
+    return average;
 }
 
-int[,] array2D = new int[3,3];
+int[,] array2D = new int[3, 3];
 SetArray2D(array2D);
 PrintArray(array2D);
-Console.WriteLine();
-Task09(array2D);
-
+Console.WriteLine($"Ср.арифм. массива = {Task08(array2D),0:f}");
