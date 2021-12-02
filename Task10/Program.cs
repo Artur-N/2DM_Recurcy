5907d07b6ca8d5ad1d56b3467f4ea8f2397db2f0
Artur <arturnl@ya.ru>
2021-12-03 01:11:15 +0300

    Task 10

---
+++ Task10/Program.cs
@@ -0,0 +1,48 @@
+﻿// 10. Дан целочисленный двумерный массив, размерности n х m. Выяснить, в какой строке последовательность является возрастающей или убывающей.
+
+Random rnd = new Random();
+void SetArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(1, 10);
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j],3}");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Task10(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j + 1] > array[i, j])
+            {
+                Console.WriteLine("Последовательность возрастающая");
+            break;
+            }
+            else if (array[i, j + 1] < array[i, j])
+            {    Console.WriteLine("Последовательность убывающая");
+            break;
+            }
+        }
+    }
+}
+
+int[,] array2D = new int[3, 3];
+SetArray2D(array2D);
+PrintArray(array2D);
+Task10(array2D);
