34ca08417348a7c5f2ce9c292de43d2f2763348e
Artur <arturnl@ya.ru>
2021-12-03 00:01:24 +0300

    Task 09

---
+++ Task09/Program.cs
@@ -0,0 +1,50 @@
+﻿// 9. Дан целочисленный двумерный массив, размерности n х m. Выяснить, какое число встречается в какой строке раньше — положительное или отрицательное.
+
+Random rnd = new Random();
+void SetArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(-10, 10);
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j],3}");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Task09(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i,j] > 0)
+            {
+                Console.WriteLine($"В {i+1}-й строке раньше встречается положительное число");
+                break;
+            }
+            else if (array[i,j] < 0)
+            {
+                Console.WriteLine($"В {i+1}-й строке раньше встречается отрицательное число");
+                break;
+            }
+        }
+    }
+}
+
+int[,] array2D = new int[3,3];
+SetArray2D(array2D);
+PrintArray(array2D);
+Console.WriteLine();
+Task09(array2D);
