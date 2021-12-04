82146cb40d74c8f73c1d4a810ac87a13ae728a8a
Artur <arturnl@ya.ru>
2021-12-04 23:55:15 +0300

    Task 48

---
+++ Task48/Program.cs
@@ -0,0 +1,28 @@
+﻿// 48. Показать двумерный массив размером m×n заполненный целыми числами
+
+Random rnd = new Random();
+void FillArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(1, 10);
+        }
+    }
+}
+void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j],3}");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array2d = new int[3, 3];
+FillArray2D(array2d);
+PrintArray2D(array2d);
