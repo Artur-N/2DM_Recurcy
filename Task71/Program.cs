bfd72c36d4ac3db5085874795d809200d9104f09
Artur <arturnl@ya.ru>
2021-12-08 03:17:30 +0300

    Task 71

---
+++ Task71/Program.cs
@@ -0,0 +1,12 @@
+﻿int i = 0;
+int A(int m, int n)
+{
+    i++;
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return A(m - 1, 1);
+    if (m > 0 && n > 0) return A(m - 1, A(m, n - 1));
+    return A(m, n);
+}
+
+System.Console.WriteLine(A(3, 10));
+System.Console.WriteLine(i);
