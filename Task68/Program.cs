e6f2101f4a57f3933d78bf21c06976680a41e6e5
Artur <arturnl@ya.ru>
2021-12-11 03:11:39 +0300

    Task 68

---
+++ Task68/Program.cs
@@ -0,0 +1,12 @@
+﻿// 68 Показать натуральные числа от M до N, N и M заданы
+
+string NumbersMN(int a, int b)
+{
+    if (a < b) return $"{a} " + NumbersMN(a + 1, b);
+    else if (a > b) return $"{a} " + NumbersMN(a - 1, b);
+    return $"{b}";
+}
+
+int M = 20;
+int N = 5;
+System.Console.WriteLine(NumbersMN(M, N));
