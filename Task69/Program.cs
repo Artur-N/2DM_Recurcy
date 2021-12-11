5256369ba089c0437a3f452747f4a04244f94b5b
Artur <arturnl@ya.ru>
2021-12-11 22:05:37 +0300

    Task 69

---
+++ Task69/Program.cs
@@ -0,0 +1,11 @@
+﻿// 69 Найти сумму элементов от M до N, N и M заданы
+
+int Summa(int m, int n)
+{
+    if (m==n) return n;
+    else return m + Summa((m+1), n);
+}
+
+int M = 1;
+int N = 10;
+System.Console.WriteLine(Summa(M,N));
