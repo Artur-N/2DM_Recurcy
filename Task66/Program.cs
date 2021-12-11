d12b66462a28e9cf751674ac34640a08899d7e06
Artur <arturnl@ya.ru>
2021-12-11 03:11:19 +0300

    Task 66

---
+++ Task66/Program.cs
@@ -0,0 +1,8 @@
+﻿// 66 Показать натуральные числа от 1 до N, N задано
+
+string Natural1N(int a)
+{
+    if (a == 1) return $"{a} ";
+    else return Natural1N(a - 1) + $"{a} ";
+}
+Console.WriteLine(Natural1N(10));
