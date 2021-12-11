c04930accff05992a9e4b0f060c9ac9a687bcf12
Artur <arturnl@ya.ru>
2021-12-11 22:37:16 +0300

    Task 70

---
+++ Task70/Program.cs
@@ -0,0 +1,9 @@
+﻿// 70 Найти сумму цифр числа
+
+int SummaNumber(int n)
+{
+    if (n == 0) return 0;
+    else return n % 10 + SummaNumber(n / 10);
+}
+
+System.Console.WriteLine(SummaNumber(12345));
