94474a7855ae9c447a9acbf1794401e9472ddaef
Artur <arturnl@ya.ru>
2021-12-13 12:05:52 +0300

    fibonacci

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,13 @@
+﻿// Фибоначчи
+
+int Fibonacci(int n)
+{
+    if (n == 0 || n == 1) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+int N = 10;
+for (int i = 0; i < N; i++)
+{
+    Console.Write($"{Fibonacci(i)} ");
+}
